8f58e1e0c1c14d50f1cf4988087fca9352d1081d
Mule129 <a24cc410s@naver.com>
2022-08-17 14:59:18 +0900

    file edit

--- User/우성민/data set test.py
+++ User/우성민/data set test.py
@@ -27,7 +27,7 @@ pose = mp_pose.Pose(
 
 #캠이 열려있는 동안 
 while cap.isOpened():
-    #idx, action 이 actions 에 존재 한다면
+    #idx, action 이 actions 에 존재 한다면 < - for문은 actions의 값을 idx, action에 할당해주는것(반대로됨)
     for idx, action in enumerate(actions):
 
         data=[]
@@ -35,10 +35,10 @@ while cap.isOpened():
         #ret, img 에 카매라를 읽은걸 저장해 주고
         ret, img = cap.read()
 
-        img = cv2.flip(img, 1)
-
+        img = cv2.flip(img, 1)#이미지 뒤집는, -1은 양 축, 1은 x축
+        #cvw.puttext 한글 깨짐문제 있음. 한글 ttf 타입 글꼴을 시스템 경로에 추가하거나 영어로 바꿔야함
         cv2.putText(img, f'준비하는동안 기다려 주세요 {action.upper()} 액션을 위한...', org=(10, 30), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-        cv2.imshow('img', img)
+        cv2.imshow('img', img)#cv2.imshow(창 이름, 창 내의 이미지(영상))
         cv2.waitKey(3000)
 
         #시작 시간을 현재 시간으로 설정해 준다
@@ -46,20 +46,21 @@ while cap.isOpened():
         #현재 시간을 시작 시간으로 뺐을 때 처음에 지정해준 숫자보다 작을 경우
         while time.time() - start_time < secs_for_action:
             #이제 몸짓을 데이터 화 시키는 작업을 업로드 할 예정
-            ret, img = cap.read()
+            ret, img = cap.read()#비디오 이미지 반환(읽기), ret에는 상태, img에는 영상/이미지 값
 
-            img = cv2.flip(img, 1)
+            img = cv2.flip(img, 1)#38번줄이랑 중복
             #이미지를 RGB형식으로 변환
             img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
             result = pose.process(img)
             #이미지를 BRG형식으로 변환
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-            
-            if result.POSE_LANDMARKS is not None:
-                for res in result.POSE_LANDMARKS:
+            """
+            #https://github.com/google/mediapipe/issues/3191 참고
+            if result.pose_landmarks is not None:
+                for res in result.pose_landmarks:
                     joint = np.zeros((33, 6))#np.zeros는 0으로 가득한 함수 생성
 
-
+            """
                 ################################################################
 
 
