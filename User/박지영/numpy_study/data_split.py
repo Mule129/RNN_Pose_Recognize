b1f155bfe866061bb4dc0256714706e4111c3853
Mule129 <a24cc410s@naver.com>
2022-08-24 16:30:46 +0900

    update

---
+++ User/박지영/numpy_study/data_split.py
@@ -0,0 +1,4 @@
+from sklearn.model_selection import train_test_split
+import numpy
+
+
---
+++ User/박지영/numpy_study/test.py
@@ -0,0 +1,13 @@
+for i in range(10):
+    for x in range(10):
+        data = input()
+        for j in range(10):
+            
+            if data == "y":
+                break
+            else:
+                pass
+            
+            print("j_", j)
+        print("x_", x)
+    print("i_", i)
