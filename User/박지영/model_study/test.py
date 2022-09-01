bcb5cdcac50997dc045b41dbff6f606e5da2f7f0
Mule129 <a24cc410s@naver.com>
2022-09-01 10:16:45 +0900

    update_make folder

--- User/박지영/model_study/test.py
+++ User/박지영/model_study/test.py
@@ -5,7 +5,7 @@ from keras.layers import LSTM, Dense, Embedding, Flatten, TimeDistributed
 from sklearn.model_selection import train_test_split
 
 model = Sequential()
-action = {"front" : 0, "back" : 1}
+action = {"front" : 0, "back" : 1, "right" : 2, "left" : 3, "jump" : 4}
 path = r"2022_AI_PJ\scr\data\move_data\data_collet"
 
 x_data, y_data = [], []
@@ -23,17 +23,10 @@ for action_1 in action:
             print(np.array(dump_1).shape)
         sell_data.append(dump_1)
         dump_1 = []
-        """for x in range(len(np_data[0])):
-            np_data = np.array(np_data[x]).flatten()"""
-        #dump_1.append(np_data)
         y_ddata.append(action[action_1])
-    
-    #y_ddata.append(dump_2)
-    #dump_2 = []
-print(y_ddata)
+
 print(np.array(y_ddata).shape)
-"""print(f"sell_data : {sell_data}, shape : {np.array(sell_data).shape}")
-print(f"y_data : {y_ddata}, shape : {np.array(y_ddata).shape}")"""
+
 print(np.array(sell_data).shape)
 x_data = np.asanyarray(sell_data)
 y_data = y_ddata
@@ -42,22 +35,23 @@ print("x : ", x_data.shape)
 print("y : ", y_data.shape)
 
 from keras.utils import to_categorical
-y_data = to_categorical(y_data, 2)
+y_data = to_categorical(y_data, len(action))
+x_data = x_data.astype(np.float32)
+y_data = y_data.astype(np.float32)
 
 x_train, x_test, y_train, y_test = train_test_split(x_data, y_data, random_state= 42)
 print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
 
 
 model = Sequential([
-    LSTM(20, return_sequences=True, input_shape=(30, 132)),
-    LSTM(20, return_sequences=True), 
-    TimeDistributed(Dense(1))])
+    LSTM(50, activation= "sigmoid", input_shape=(30, 132)),
+    Dense(10), 
+    Dense(5, "softmax")])
 
 model.summary()
 
 model.compile(optimizer="adam",
-loss="sparse_categorical_crossentropy",
-metrics=["accuracy"])
+loss="mse", epochs=30, batch_size=10, validation_data = (x_test, y_test))
 
 model.fit(x_train, y_train, epochs=30)
 model.summary()
